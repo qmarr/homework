a38354a8187cbea53da7c1e713283681e07c124a
Vlad Komarenets <komarenetsv@gmail.com>
2026-02-16 21:21:14 +0200

    made police blinker

--- esp32blink/src/main.cpp
+++ esp32blink/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+typedef uint8_t pin_t;
+void policeBlink(pin_t, pin_t);
+constexpr pin_t LEDS_COUNT = 2;
+constexpr pin_t LED_PINS[LEDS_COUNT]{17, 18};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  for (size_t i = 0; i < LEDS_COUNT; i++)
+  {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  policeBlink(LED_PINS[0], LED_PINS[1]);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void policeBlink(pin_t firstPin, pin_t secondPin) {
+
+  digitalWrite(firstPin, HIGH);
+  delay(500);
+
+  digitalWrite(firstPin, LOW);
+  digitalWrite(secondPin, HIGH);
+  delay(500);
+
+  digitalWrite(secondPin, LOW);
 }
