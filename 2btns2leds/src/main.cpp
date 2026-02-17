e65cf54fc3186ae634935e0fed7f6fb22067d547
Vlad Komarenets <komarenetsv@gmail.com>
2026-02-17 23:55:56 +0200

    init from prev project, added two pins for btns

---
+++ 2btns2leds/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+typedef uint8_t pin_t;
+void policeBlink(pin_t, pin_t);
+constexpr pin_t LEDS_COUNT = 2;
+constexpr pin_t LED_PINS[LEDS_COUNT]{17, 18};
+constexpr pin_t BTN_PIN = 5;
+constexpr pin_t BOOT_BTN = 0;
+
+void setup() {
+  
+  for (size_t i = 0; i < LEDS_COUNT; i++)
+  {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+  pinMode(BTN_PIN, INPUT);
+}
+
+void loop() {
+  policeBlink(LED_PINS[0], LED_PINS[1]);
+}
+
+
+void policeBlink(pin_t firstPin, pin_t secondPin) {
+
+  digitalWrite(firstPin, HIGH);
+  delay(500);
+
+  digitalWrite(firstPin, LOW);
+  digitalWrite(secondPin, HIGH);
+  delay(500);
+
+  digitalWrite(secondPin, LOW);
+}
