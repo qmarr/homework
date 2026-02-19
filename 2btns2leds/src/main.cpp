311d1df75c3d30e2dde6e742b425fb7521dd748e
Vlad Komarenets <komarenetsv@gmail.com>
2026-02-19 03:29:01 +0200

    added debounce

--- 2btns2leds/src/main.cpp
+++ 2btns2leds/src/main.cpp
@@ -6,63 +6,83 @@ constexpr pin_t LEDS_COUNT = 2;
 constexpr pin_t LED_PINS[LEDS_COUNT]{17, 18};
 constexpr pin_t BTN_PIN = 5;
 constexpr pin_t BOOT_BTN = 0;
-bool btnPressed = false;
-bool btnState = false;
-bool lastBtnState = LOW;
 
-bool bootBtnPressed = false;
-bool bootBtnState = false;
-bool lastBootBtnState = HIGH;
+struct Button
+{
+  bool pressed;
+  bool state;
+  bool lastState;
+  const pin_t btnPin;
+  uint16_t lastDebounceTime = 0;
+  uint16_t debounceDelay = 50;
 
-void setup() {
-  
+  Button(bool pressed, bool state, bool lastState, pin_t pin)
+      : pressed(pressed), state(state), lastState(lastState), btnPin(pin)
+  {
+  }
+};
+
+Button outBtn(false, false, HIGH, BTN_PIN);
+Button bootBtn(false, false, HIGH, BOOT_BTN);
+
+void setup()
+{
+
   for (size_t i = 0; i < LEDS_COUNT; i++)
   {
     pinMode(LED_PINS[i], OUTPUT);
     digitalWrite(LED_PINS[i], LOW);
   }
-  pinMode(BTN_PIN, INPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
   pinMode(BOOT_BTN, INPUT_PULLUP);
   Serial.begin(115200);
 }
 
-void loop() {
+void btnHandler(Button &btn)
+{
+  uint16_t reading = digitalRead(btn.btnPin);
 
-  btnState = digitalRead(BTN_PIN);
-
-  if(btnState == HIGH && lastBtnState == LOW) {
- 
-    btnPressed = !btnPressed;
-    Serial.print("btn toggled: ");
-    Serial.println(btnState);
+  if (reading != btn.lastState) 
+  {
+    btn.lastDebounceTime = millis();
   }
 
-  lastBtnState = btnState;
-
-  bootBtnState = digitalRead(BOOT_BTN);
-  if(bootBtnState == LOW && lastBootBtnState == HIGH) {
- 
-    bootBtnPressed = !bootBtnPressed;
-    Serial.print("boot toggled: ");
-    Serial.println(bootBtnState);
+  if ((millis() - btn.lastDebounceTime) > btn.debounceDelay)
+  {
+    if (reading != btn.state)
+    {
+      btn.state = reading;
+      if(btn.state == LOW) {
+        btn.pressed = !btn.pressed;
+        Serial.println("pressed");
+      }
+    }
   }
-
-  lastBootBtnState = bootBtnState;
-
-  if(btnPressed) {
- 
-    policeBlink(LED_PINS[0], LED_PINS[1], 200);
-  } else if(bootBtnPressed) {
- 
-    policeBlink(LED_PINS[0], LED_PINS[1], 500);
-  }
-    
-  
-
+  btn.lastState = reading;
 }
 
+void loop()
+{
 
-void policeBlink(pin_t firstPin, pin_t secondPin, uint16_t milisecondsDelay) {
+  btnHandler(outBtn);
+  btnHandler(bootBtn);
+
+  if (outBtn.pressed)
+  {
+
+    policeBlink(LED_PINS[0], LED_PINS[1], 200);
+  }
+  else if (bootBtn.pressed)
+  {
+
+    policeBlink(LED_PINS[0], LED_PINS[1], 500);
+  }
+
+  // todo get rid of delay
+}
+
+void policeBlink(pin_t firstPin, pin_t secondPin, uint16_t milisecondsDelay)
+{
 
   digitalWrite(firstPin, HIGH);
   delay(milisecondsDelay);
