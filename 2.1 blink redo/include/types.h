a7e88447fbf7222380fa2930c363f9e95c326275
Vlad Komarenets <komarenetsv@gmail.com>
2026-03-04 23:59:12 +0200

    Fix header guard in types.h to prevent multiple inclusions

--- 2.1 blink redo/include/types.h
+++ 2.1 blink redo/include/types.h
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 
+#ifndef TYPES_H
+#define TYPES_H
+
 typedef uint8_t pin_t;
 
 constexpr pin_t LED_PIN{5};
@@ -9,4 +12,7 @@ enum class BLINK_SPEED
     FAST = 200,
     MEDIUM = 400,
     SLOW = 800,
+    SLUGGISH = 1200
 };
+
+#endif
