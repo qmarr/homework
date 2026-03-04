7e473e319689d6544d32752d273167f5e6d80a8f
Vlad Komarenets <komarenetsv@gmail.com>
2026-03-04 23:59:27 +0200

    Refactor main.cpp to move LED blinking logic into a separate function and adjust setup for improved clarity

--- 2.1 blink redo/src/main.cpp
+++ 2.1 blink redo/src/main.cpp
@@ -1,15 +1,30 @@
 #include "led.h"
+#include "config.h"
 
 void setup()
 {
   Serial.begin(115200);
-  Led led1;
 }
+
+void blink(const Config &config, Led &ledToBlink)
+{
+  uint16_t curMillis = millis();
+  static uint16_t lastTick{0};
+
+  if (curMillis - lastTick > config.getSpeed())
+  {
+    lastTick = curMillis;
+    digitalWrite(ledToBlink.getLedPin(), ledToBlink.makeStateOpposite());
+  }
+}
+
+Led led1;
+const Config conf1;
 
 void loop()
 {
 
-
+  blink(conf1, led1);
 
   Serial.print("Millis: ");
   Serial.println(millis());
