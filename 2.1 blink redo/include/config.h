88cb82cf3fbbd7a6abc395e59e5c5ff29fe7d88d
Vlad Komarenets <komarenetsv@gmail.com>
2026-03-04 23:59:00 +0200

    Implement Config class with speed management and retrieval for improved blink control

--- 2.1 blink redo/include/config.h
+++ 2.1 blink redo/include/config.h
@@ -1,1 +1,42 @@
+#include "types.h"
 
+class Config
+{
+private:
+    BLINK_SPEED speed;
+
+public:
+    Config();
+    Config(BLINK_SPEED spd);
+    ~Config();
+    uint16_t getSpeed() const;
+};
+
+Config::Config() : speed(BLINK_SPEED::SLUGGISH)
+{
+}
+
+inline Config::Config(BLINK_SPEED spd)
+{
+    speed = spd;
+}
+
+Config::~Config()
+{
+}
+
+inline uint16_t Config::getSpeed() const
+{
+    switch (speed)
+    {
+    case BLINK_SPEED::FAST:
+        return 200;
+    case BLINK_SPEED::MEDIUM:
+        return 400;
+    case BLINK_SPEED::SLOW:
+        return 800;
+    default:
+        return 1200;
+    }
+}
+ 
