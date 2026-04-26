5e140f8c7d60d9a3e73876c65a20d315e2e4fd03
Vlad Komarenets <komarenetsv@gmail.com>
2026-04-27 02:42:26 +0300

    Add SSD1306 driver implementation and header; include character drawing functions

--- 4.3_I2C_OLED/src/main.c
+++ 4.3_I2C_OLED/src/main.c
@@ -5,124 +5,17 @@
 #include "driver/gpio.h"
 #include "driver/i2c_master.h"
 #include "font5x7.h"
+#include "ssd1306.h"
 
 #define I2C_MASTER_SCL_IO GPIO_NUM_9
 #define I2C_MASTER_SDA_IO GPIO_NUM_8
 #define I2C_MASTER_NUM I2C_NUM_0 // I2C порт 0
 #define I2C_MASTER_FREQ_HZ 100000
 #define OLED_ADDR 0x3C
- 
 
 static i2c_master_bus_handle_t bus_handle;
 static i2c_master_dev_handle_t dev_handle;
 
-static esp_err_t ssd1306_cmd(i2c_master_dev_handle_t dev, uint8_t cmd)
-{
-    uint8_t buf[2] = {0x00, cmd};
-    return i2c_master_transmit(dev, buf, sizeof(buf), -1);
-}
-static esp_err_t ssd1306_cmd2(i2c_master_dev_handle_t dev, uint8_t cmd, uint8_t arg)
-{
-    uint8_t buf[3] = {0x00, cmd, arg};
-    return i2c_master_transmit(dev, buf, sizeof(buf), -1);
-}
-
-static esp_err_t ssd1306_data(i2c_master_dev_handle_t dev, const uint8_t *data, size_t len)
-{
-    uint8_t buf[129];
-    if (len > 128)
-    {
-        return ESP_ERR_INVALID_SIZE;
-    }
-
-    buf[0] = 0x40;
-
-    for (size_t i = 0; i < len; i++)
-    {
-        buf[i + 1] = data[i];
-    }
-
-    return i2c_master_transmit(dev, buf, len + 1, -1);
-}
-
-static void ssd1306_init(i2c_master_dev_handle_t dev)
-{
-    ssd1306_cmd(dev, 0xAE); // Display OFF
-
-    ssd1306_cmd2(dev, 0xD5, 0x80); // Clock divide
-    ssd1306_cmd2(dev, 0xA8, 0x3F); // Multiplex ratio 1/64
-    ssd1306_cmd2(dev, 0xD3, 0x00); // Display offset
-    ssd1306_cmd(dev, 0x40);        // Display start line
-
-    ssd1306_cmd2(dev, 0x8D, 0x14); // Charge pump ON
-    ssd1306_cmd2(dev, 0x20, 0x00); // Horizontal addressing mode
-
-    ssd1306_cmd(dev, 0xA1); // Segment remap
-    ssd1306_cmd(dev, 0xC8); // COM scan direction remapped
-
-    ssd1306_cmd2(dev, 0xDA, 0x12); // COM pins config for 128x64
-    ssd1306_cmd2(dev, 0x81, 0x7F); // Contrast
-
-    ssd1306_cmd2(dev, 0xD9, 0xF1); // Pre-charge
-    ssd1306_cmd2(dev, 0xDB, 0x40); // VCOMH deselect
-
-    ssd1306_cmd(dev, 0xA4); // Resume RAM content
-    ssd1306_cmd(dev, 0xA6); // Normal display
-    ssd1306_cmd(dev, 0xAF); // Display ON
-}
-
-static void ssd1306_set_full_area(i2c_master_dev_handle_t dev)
-{
-    ssd1306_cmd(dev, 0x21); // Set column address
-    ssd1306_cmd(dev, 0);    // Start column
-    ssd1306_cmd(dev, 127);  // End column
-
-    ssd1306_cmd(dev, 0x22); // Set page address
-    ssd1306_cmd(dev, 0);    // Start page
-    ssd1306_cmd(dev, 7);    // End page
-}
-
-static void ssd1306_clear(i2c_master_dev_handle_t dev)
-{
-    uint8_t zeros[128] = {0};
-
-    ssd1306_set_full_area(dev);
-
-    for (int page = 0; page < 8; ++page)
-    {
-        ESP_ERROR_CHECK(ssd1306_data(dev, zeros, sizeof(zeros)));
-    }
-}
-
-static const uint8_t *get_char_bitmap(char c)
-{
-    if (c < 32 || c > 127)
-    {
-        c = ' ';
-    }
-
-    return font5x7[c - 32];
-}
-
-static void ssd1306_draw_char(i2c_master_dev_handle_t dev, char c)
-{
-    const uint8_t *bitmap = get_char_bitmap(c);
-
-    ESP_ERROR_CHECK(ssd1306_data(dev, bitmap, 5));
-
-    uint8_t spacing = 0x00;
-    ESP_ERROR_CHECK(ssd1306_data(dev, &spacing, 1));
-}
-
-static void ssd1306_draw_string(i2c_master_dev_handle_t dev, const char *str)
-{
-
-    while (*str)
-    {
-        ssd1306_draw_char(dev, *str);
-        str++;
-    }
-}
 
 void i2c_init()
 {
@@ -145,27 +38,13 @@ void i2c_init()
 
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
 }
-
-static void ssd1306_set_cursor(i2c_master_dev_handle_t dev, uint8_t page, uint8_t col)
-{
-    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0xB0 | page));                // page 0..7
-    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0x00 | (col & 0x0F)));        // lower column
-    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0x10 | ((col >> 4) & 0x0F))); // higher column
-}
-
-static void ssd1306_print_at(i2c_master_dev_handle_t dev, uint8_t page, uint8_t col, const char *text)
-{
-    ssd1306_set_cursor(dev, page, col);
-    ssd1306_draw_string(dev, text);
-}
-
 void app_main()
 {
     i2c_init();
     ssd1306_init(dev_handle);
     ssd1306_clear(dev_handle);
 
-    ssd1306_print_at(dev_handle, 3, 20, "Salut, Zooble");
+    ssd1306_print_at(dev_handle, 3, 20, "Salut, Zooble!");
 
     while (1)
     {
---
+++ 4.3_I2C_OLED/src/ssd1306.c
@@ -0,0 +1,130 @@
+#include "ssd1306.h"
+
+esp_err_t ssd1306_cmd(i2c_master_dev_handle_t dev, uint8_t cmd)
+{
+    uint8_t buf[2] = {0x00, cmd};
+    return i2c_master_transmit(dev, buf, sizeof(buf), -1);
+}
+esp_err_t ssd1306_cmd2(i2c_master_dev_handle_t dev, uint8_t cmd, uint8_t arg)
+{
+    uint8_t buf[3] = {0x00, cmd, arg};
+    return i2c_master_transmit(dev, buf, sizeof(buf), -1);
+}
+
+esp_err_t ssd1306_data(i2c_master_dev_handle_t dev, const uint8_t *data, size_t len)
+{
+    uint8_t buf[129];
+    if (len > 128)
+    {
+        return ESP_ERR_INVALID_SIZE;
+    }
+
+    buf[0] = 0x40;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        buf[i + 1] = data[i];
+    }
+
+    return i2c_master_transmit(dev, buf, len + 1, -1);
+}
+
+void ssd1306_init(i2c_master_dev_handle_t dev)
+{
+    ssd1306_cmd(dev, 0xAE); // Display OFF
+
+    ssd1306_cmd2(dev, 0xD5, 0x80); // Clock divide
+    ssd1306_cmd2(dev, 0xA8, 0x3F); // Multiplex ratio 1/64
+    ssd1306_cmd2(dev, 0xD3, 0x00); // Display offset
+    ssd1306_cmd(dev, 0x40);        // Display start line
+
+    ssd1306_cmd2(dev, 0x8D, 0x14); // Charge pump ON
+    ssd1306_cmd2(dev, 0x20, 0x00); // Horizontal addressing mode
+
+    ssd1306_cmd(dev, 0xA1); // Segment remap
+    ssd1306_cmd(dev, 0xC8); // COM scan direction remapped
+
+    ssd1306_cmd2(dev, 0xDA, 0x12); // COM pins config for 128x64
+    ssd1306_cmd2(dev, 0x81, 0x7F); // Contrast
+
+    ssd1306_cmd2(dev, 0xD9, 0xF1); // Pre-charge
+    ssd1306_cmd2(dev, 0xDB, 0x40); // VCOMH deselect
+
+    ssd1306_cmd(dev, 0xA4); // Resume RAM content
+    ssd1306_cmd(dev, 0xA6); // Normal display
+    ssd1306_cmd(dev, 0xAF); // Display ON
+}
+
+void ssd1306_set_full_area(i2c_master_dev_handle_t dev)
+{
+    ssd1306_cmd(dev, 0x21); // Set column address
+    ssd1306_cmd(dev, 0);    // Start column
+    ssd1306_cmd(dev, 127);  // End column
+
+    ssd1306_cmd(dev, 0x22); // Set page address
+    ssd1306_cmd(dev, 0);    // Start page
+    ssd1306_cmd(dev, 7);    // End page
+}
+
+void ssd1306_clear(i2c_master_dev_handle_t dev)
+{
+    uint8_t zeros[128] = {0};
+
+    ssd1306_set_full_area(dev);
+
+    for (int page = 0; page < 8; ++page)
+    {
+        ESP_ERROR_CHECK(ssd1306_data(dev, zeros, sizeof(zeros)));
+    }
+}
+
+const uint8_t *get_char_bitmap(char c)
+{
+    if (c < 32 || c > 127)
+    {
+        c = ' ';
+    }
+
+    return font5x7[c - 32];
+}
+
+void ssd1306_draw_char(i2c_master_dev_handle_t dev, char c)
+{
+    const uint8_t *bitmap = get_char_bitmap(c);
+
+    ESP_ERROR_CHECK(ssd1306_data(dev, bitmap, 5));
+
+    uint8_t spacing = 0x00;
+    ESP_ERROR_CHECK(ssd1306_data(dev, &spacing, 1));
+}
+
+void ssd1306_draw_string(i2c_master_dev_handle_t dev, const char *str)
+{
+
+    while (*str)
+    {
+        ssd1306_draw_char(dev, *str);
+        str++;
+    }
+}
+
+void ssd1306_set_cursor(i2c_master_dev_handle_t dev, uint8_t page, uint8_t col)
+{
+    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0xB0 | page));                // page 0..7
+    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0x00 | (col & 0x0F)));        // lower column
+    ESP_ERROR_CHECK(ssd1306_cmd(dev, 0x10 | ((col >> 4) & 0x0F))); // higher column
+}
+
+void ssd1306_print_at(i2c_master_dev_handle_t dev, uint8_t page, uint8_t col, const char *text)
+{
+    ssd1306_set_cursor(dev, page, col);
+    ssd1306_draw_string(dev, text);
+}
+
+void ssd1306_clear_line(i2c_master_dev_handle_t dev, uint8_t page)
+{
+    uint8_t zeros[128] = {0x00};
+
+    ssd1306_set_cursor(dev, page, 0);
+    ESP_ERROR_CHECK(ssd1306_data(dev, zeros, sizeof(zeros)));
+}
